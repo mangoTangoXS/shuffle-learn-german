669533e8983cc03beb7be7b7027d4ea5a436a5b1
mangoTango <karolina.beata.zak@gmail.com>
2020-07-02 11:11:40 +0200

    Depending on the request (guessing in English or German) data returned in createResponse method are updated separately for chosen language

--- src/main/java/com/panda/corp/shuffle/ShuffleWordService.java
+++ src/main/java/com/panda/corp/shuffle/ShuffleWordService.java
@@ -1,16 +1,15 @@
 package com.panda.corp.shuffle;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
 @Service
 public class ShuffleWordService {
+
+    private static final String ENGLISH = "ENG";
+    private static final String GERMAN = "DE";
 
     private Map<String, String> wordsCollectionDE;
     private Map<String, String> wordsCollectionENG;
@@ -18,35 +17,33 @@ public class ShuffleWordService {
 
     // private Set<String> guessedWords;
     private Random random;
-    //    private ShuffleWordDatabaseMock database = new ShuffleWordDatabaseMock();
     private ShuffleWordDatabase database;
-    private int numberOfNotGuessed;
+    private int numberOfNotGuessedDE;
+    private int numberOfNotGuessedENG;
 
     @Autowired
     public ShuffleWordService(ShuffleWordDatabase shuffleWordDatabase) {
         this.database = shuffleWordDatabase;
 
-        wordsCollectionDE = new HashMap<>();
-        wordsCollectionENG = new HashMap<>();
+        wordsCollectionDE = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+        wordsCollectionENG = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
 
         database.findAll().forEach(vocabEntity -> wordsCollectionDE.put(vocabEntity.getKey(), vocabEntity.getValue()));
         database.findAll().forEach(vocabEntity -> wordsCollectionENG.put(vocabEntity.getValue(), vocabEntity.getKey()));
 
-        difficultWords = new HashMap<>(10);
+        difficultWords = new HashMap<>();
         random = new Random();
     }
 
     String getWord(String language) {
         switch (language) {
-            case "ENG":
+            case ENGLISH:
                 return getWordToGuesses(wordsCollectionDE);
-            case "DE":
+            case GERMAN:
                 return getWordToGuesses(wordsCollectionENG);
             default:
                 return "wrong input";
-
         }
-
     }
 
     private String getWordToGuesses(Map<String, String> wordsCollection) {
@@ -59,9 +56,9 @@ public class ShuffleWordService {
 
     ShuffleResponseDTO getLanguageToBeChecked(String language, String key, String inputValue) {
         switch (language) {
-            case "ENG":
+            case ENGLISH:
                 return checkIfCorrectAnswer(key, inputValue, wordsCollectionDE);
-            case "DE":
+            case GERMAN:
                 return checkIfCorrectAnswer(key, inputValue, wordsCollectionENG);
             default:
                 throw new RuntimeException("Wrong input");
@@ -69,23 +66,31 @@ public class ShuffleWordService {
     }
 
     private ShuffleResponseDTO checkIfCorrectAnswer(String key, String inputValue, Map<String, String> wordsCollection) {
-
         if (!wordsCollection.containsKey(key)) {
-            numberOfNotGuessed++;
-            return createResponse(false, wordsCollection);
+            if (wordsCollection.equals(wordsCollectionDE)) {
+                return createResponse(false, wordsCollection, numberOfNotGuessedDE);
+            } else {
+                numberOfNotGuessedENG++;
+                return createResponse(false, wordsCollection, numberOfNotGuessedENG);
+            }
         }
 
-        if (inputValue.equals(wordsCollection.get(key))) {
+        if (inputValue.equalsIgnoreCase(wordsCollection.get(key))) {
             wordsCollection.remove(key);
-            return createResponse(true, wordsCollection);
+            return createResponse(true, wordsCollection, 0);
         } else {
-            difficultWords.put(key, wordsCollection.get(key));
-            numberOfNotGuessed++;
-            return createResponse(false, wordsCollection);
+            difficultWords.put(key, wordsCollection.get(key)); //to be distinguished if ENG or DE
+            if (wordsCollection.equals(wordsCollectionDE)) {
+                numberOfNotGuessedDE++;
+                return createResponse(false, wordsCollection, numberOfNotGuessedDE);
+            } else {
+                numberOfNotGuessedENG++;
+                return createResponse(false, wordsCollection, numberOfNotGuessedDE);
+            }
         }
     }
 
-    private ShuffleResponseDTO createResponse(boolean result, Map<String, String> wordsCollection) {
+    private ShuffleResponseDTO createResponse(boolean result, Map<String, String> wordsCollection, int numberOfNotGuessed) {
         int numberOfCorrectAnswers = (int) (database.count() - wordsCollection.size());
         return ShuffleResponseDTO.of(result, numberOfCorrectAnswers, numberOfNotGuessed, wordsCollection.size());
     }
